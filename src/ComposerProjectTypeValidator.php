2a917e18d26184b1dd9c2dccd5c99c5184299a5d
alanvdb <alanvdb.coder@gmail.com>
2025-05-22 13:14:34 +0200

    Allow custom types

--- src/ComposerProjectTypeValidator.php
+++ src/ComposerProjectTypeValidator.php
@@ -9,30 +9,16 @@ class ComposerProjectTypeValidator
     extends AbstractValidator
     implements ValidatorInterface
 {
-    const TYPES = [
-        'library',
-        'project',
-        'metapackage',
-        'composer-plugin',
-        'wordpress-plugin',
-        'wordpress-theme',
-        'phpcodesniffer-standard',
-        'drupal-module',
-        'drupal-theme',
-        'drupal-profile',
-        'drupal-drush',
-    ];
-
     public function __construct(string $errorMessage = '')
     {
         if (empty($errorMessage)) {
-            $errorMessage = "Invalid project type provided. Valid types are: " . implode(', ', self::TYPES) . ".";
+            $errorMessage = "The project type must be a valid string containing only lowercase letters, numbers, and hyphens.";
         }
         parent::__construct($errorMessage);
     }
 
     public function isValid(mixed $value) : bool
     {
-        return is_string($value) ? in_array($value, self::TYPES) : false;
+        return (bool) preg_match('/^[a-z0-9-]+$/', $value);
     }
 }
